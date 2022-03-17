a972710148c943f8c5d6cecef94db5956348b14c
DenisFilichev <denis.filic@gmail.com>
2022-03-17 18:54:36 +0300

    Сделали вывод текста в консоль

---
+++ DEV_J110_2/src/dev_j110_2/DEV_J110_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev_j110_2;
+
+/**
+ *
+ * @author denis
+ */
+public class DEV_J110_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
